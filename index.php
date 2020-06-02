d612e3d71c88f5ef291ded9d93cf6ddf2c27008c
Samuel Durrant-Walker <SamuelDurW@outlook.com>
2020-06-02 20:56:00 +0100

    Initial Files
    Adding the inital files to work from, including the connection to database, intial page, styles and simple directory structure

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+	session_start(); 
+	require_once("connection.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Quizmaster</h1>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php echo '<p>Hello world</p>'; ?>
+
+<?php 
+	$link = mysqli_connect('localhost','Supertester', 'Pizzaham13', 'quizmaster'); 
+	if($link -> connect_errno) {
+		echo "Failed to connect to MySQL: " . $link -> connect_error;
+		exit();
+	} else {
+		echo "Successfully connected";
+	}
+?>
